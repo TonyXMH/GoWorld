aa82a3dce74fe95b8903ec6e180b353a5a97705f
tonyXinminghui <445241843@qq.com>
2018-10-18 17:32:57 +0800

    添加配置读取
    利用了gopkg.in/ini.v1做配置文件的读取。

---
+++ config/config_test.go
@@ -0,0 +1,72 @@
+package config
+
+import (
+	"../gwlog"
+	"encoding/json"
+	"testing"
+)
+
+func TestLoad(t *testing.T) {
+	config := Get()
+	gwlog.Debug("goworld config: \n%s", config)
+	if config == nil {
+		t.FailNow()
+	}
+	if config.dispatcher.Ip == "" {
+		t.Errorf("dispatch ip not found")
+	}
+	if config.dispatcher.Port == 0 {
+		t.Errorf("dispatcher port not found")
+	}
+	for gameid, gameConfig := range config.games {
+		if gameConfig.Ip == "" {
+			t.Errorf("game %d ip not found", gameid)
+		}
+		if gameConfig.Port == 0 {
+			t.Errorf("game %d port not found", gameid)
+		}
+	}
+
+	for gateid, gateConfig := range config.gates {
+		if gateConfig.Ip == "" {
+			t.Errorf("gate %d ip not found", gateid)
+		}
+		if gateConfig.Port == 0 {
+			t.Errorf("gate %d port not found", gateid)
+		}
+	}
+	gwlog.Info("read goworld config:%v", config)
+}
+func TestReload(t *testing.T) {
+	config := Get()
+	config = Reload()
+	gwlog.Debug("goworld config:\n%s", config)
+}
+
+func TestGetDispatcher(t *testing.T) {
+	cfg := GetDispatcher()
+	cfgStr, _ := json.Marshal(cfg)
+	gwlog.Info("dispatcher config:%s", string(cfgStr))
+}
+
+func TestGetGame(t *testing.T) {
+	for id := 1; id <= 10; id++ {
+		cfg := GetGame(id)
+		if cfg == nil {
+			gwlog.Error("Game %d not found", id)
+		} else {
+			gwlog.Info("Game %d config: %v", id, cfg)
+		}
+	}
+}
+
+func TestGetGate(t *testing.T) {
+	for id := 1; id <= 10; id++ {
+		cfg := GetGate(id)
+		if cfg == nil {
+			gwlog.Error("Gate %d not found", id)
+		} else {
+			gwlog.Info("Gate %d config: %v", id, cfg)
+		}
+	}
+}
---
+++ config/read_config.go
@@ -0,0 +1,145 @@
+package config
+
+import (
+	"../gwlog"
+	"fmt"
+	"gopkg.in/ini.v1"
+	"strconv"
+	"strings"
+)
+
+const (
+	DEFAULT_CONFIG_FILENAME = "goworld.ini"
+	DEFAULT_LOCALHOST_IP    = "127.0.0.1"
+)
+
+var (
+	goWorldConfig *GoWorldConfig
+)
+
+type GameConfig struct {
+	Ip   string
+	Port int
+}
+
+type DispatcherConfig struct {
+	Ip   string
+	Port int
+}
+
+type GateConfig struct {
+	Ip   string
+	Port int
+}
+
+type GoWorldConfig struct {
+	dispatcher DispatcherConfig
+	games      map[int]*GameConfig
+	gates      map[int]*GateConfig
+}
+
+func Get() *GoWorldConfig {
+	if goWorldConfig == nil {
+		goWorldConfig = readGoWorldConfig()
+	}
+	return goWorldConfig
+}
+
+func Reload() *GoWorldConfig {
+	goWorldConfig = nil
+	return Get()
+}
+
+func GetGame(gameid int) *GameConfig {
+	return Get().games[gameid]
+}
+
+func GetGate(gateid int) *GateConfig {
+	return Get().gates[gateid]
+}
+
+func GetDispatcher() *DispatcherConfig {
+	return &Get().dispatcher
+}
+
+func readGoWorldConfig() *GoWorldConfig {
+	config := GoWorldConfig{
+		games: map[int]*GameConfig{},
+		gates: map[int]*GateConfig{},
+	}
+
+	iniFile, err := ini.Load(DEFAULT_CONFIG_FILENAME)
+	checkConfigError(err, "")
+	for _, sec := range iniFile.Sections() {
+		secName := sec.Name()
+		if secName == "DEFAULT" {
+			continue
+		}
+		gwlog.Info("Section %s", sec.Name())
+		if secName == "dispatcher" {
+			readDispatcherConfig(sec, &config.dispatcher)
+		} else if secName[:4] == "game" {
+			id, err := strconv.Atoi(secName[4:])
+			checkConfigError(err, fmt.Sprintf("invalid game name:%s", secName))
+			config.games[id] = readGameConfig(sec)
+		} else if secName[:4] == "gate" {
+			id, err := strconv.Atoi(secName[4:])
+			checkConfigError(err, fmt.Sprintf("invalid gate name: %s", secName))
+			config.gates[id] = readGateConfig(sec)
+		} else {
+			gwlog.Warn("unknown section:%s", secName)
+		}
+	}
+	return &config
+}
+
+func readGameConfig(sec *ini.Section) *GameConfig {
+	gc := &GameConfig{
+		Ip: DEFAULT_LOCALHOST_IP,
+	}
+	for _, key := range sec.Keys() {
+		name := strings.ToLower(key.Name())
+		if name == "ip" {
+			gc.Ip = key.MustString(DEFAULT_LOCALHOST_IP)
+		} else if name == "port" {
+			gc.Port = key.MustInt(0)
+		}
+	}
+	return gc
+}
+
+func readGateConfig(sec *ini.Section) *GateConfig {
+	gc := &GateConfig{
+		Ip: DEFAULT_LOCALHOST_IP,
+	}
+	for _, key := range sec.Keys() {
+		name := strings.ToLower(key.Name())
+		if name == "ip" {
+			gc.Ip = key.MustString(DEFAULT_LOCALHOST_IP)
+		} else if name == "port" {
+			gc.Port = key.MustInt(0)
+		}
+	}
+	return gc
+}
+
+func readDispatcherConfig(sec *ini.Section, config *DispatcherConfig) {
+	config.Ip = DEFAULT_LOCALHOST_IP
+	for _, key := range sec.Keys() {
+		name := strings.ToLower(key.Name())
+		if name == "ip" {
+			config.Ip = key.MustString(DEFAULT_LOCALHOST_IP)
+		} else if name == "port" {
+			config.Port = key.MustInt(0)
+		}
+	}
+}
+
+func checkConfigError(err error, msg string) {
+	if err != nil {
+		if msg == "" {
+			msg = err.Error()
+		}
+		gwlog.Panicf("read config error:%s", msg)
+	}
+}
