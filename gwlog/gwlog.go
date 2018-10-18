219b4f9c2246fc0b306fd19166d0504abb5b9223
tonyXinminghui <445241843@qq.com>
2018-10-18 17:30:40 +0800

    gwlog日志包
    封装了logrus

---
+++ gwlog/gwlog.go
@@ -0,0 +1,40 @@
+package gwlog
+
+import (
+	sublog "github.com/Sirupsen/logrus"
+	"runtime/debug"
+)
+
+var (
+	DEBUG = sublog.DebugLevel
+	INFO  = sublog.InfoLevel
+	WARN  = sublog.WarnLevel
+	ERROR = sublog.ErrorLevel
+	PANIC = sublog.PanicLevel
+	FATAL = sublog.FatalLevel
+
+	Debug  = sublog.Debugf
+	Info   = sublog.Infof
+	Warn   = sublog.Warnf
+	Error  = sublog.Errorf
+	Panic  = sublog.Panic
+	Panicf = sublog.Panicf
+	Fatal  = sublog.Fatalf
+)
+
+func ParseLevel(lvl string) (sublog.Level, error) {
+	return sublog.ParseLevel(lvl)
+}
+
+func SetLevel(lv sublog.Level) {
+	sublog.SetLevel(lv)
+}
+
+func TraceError(format string, args ...interface{}) {
+	debug.PrintStack()
+	Error(format, args...)
+}
+
+func init() {
+	sublog.SetLevel(sublog.DebugLevel)
+}
