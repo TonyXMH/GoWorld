9bfff2355a53a5fbb35c5739a5dcee7f5488fe82
tonyXinminghui <445241843@qq.com>
2018-11-07 18:30:38 +0800

    添加注册服务

--- proto/GoWorldConnection.go
+++ proto/GoWorldConnection.go
@@ -20,16 +20,24 @@ func NewGoWorldConnection(conn net.Conn) GoWorldConnection {
 	}
 }
 
-func (gwc *GoWorldConnection) SetGameID(id int) error {
+func (gwc *GoWorldConnection) SendSetGameID(id int) error {
 	packet := gwc.packetConn.NewPacket()
 	packet.AppendUint16(MT_SET_GAME_ID)
 	packet.AppendUint16(uint16(id))
 	return gwc.packetConn.SendPacket(packet)
 }
-func (gwc *GoWorldConnection) NotifyCreateEntity(id entity.EntityID) error {
+func (gwc *GoWorldConnection) SendNotifyCreateEntity(id entity.EntityID) error {
 	packet := gwc.packetConn.NewPacket()
 	packet.AppendUint16(MT_NOTIFY_CREATE_ENTITY)
 	packet.AppendBytes([]byte(id))
+	return gwc.packetConn.SendPacket(packet)
+}
+
+func (gwc *GoWorldConnection) SendRegisterService(id entity.EntityID, serviceName string) error {
+	packet := gwc.packetConn.NewPacket()
+	packet.AppendUint16(MT_REGISTER_SERVICE)
+	packet.AppendBytes([]byte(id))
+	packet.AppendVarStr(serviceName)
 	return gwc.packetConn.SendPacket(packet)
 }
 
